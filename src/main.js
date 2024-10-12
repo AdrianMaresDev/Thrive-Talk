0d73a59f20f2367439969fed02854b1590636939
AdrianMares <adrianmares.dev@gmail.com>
2024-10-12 01:53:05 -0400

    added javascript for nav toggle

--- src/main.js
+++ src/main.js
@@ -1,1 +1,17 @@
-console.log("testing");
+const menuIcon = document.getElementById('menu-button');
+const closeIcon = document.getElementById('close-button');
+const navMenu = document.getElementById('nav-menu');
+
+//Navigation Menu Toggle
+
+menuIcon.addEventListener('click', () => {
+    navMenu.classList.remove('hidden');
+    menuIcon.classList.add('hidden');
+    closeIcon.classList.remove('hidden');
+});
+
+closeIcon.addEventListener('click', () => {
+    navMenu.classList.add('hidden');
+    menuIcon.classList.remove('hidden');
+    closeIcon.classList.add('hidden');
+});
