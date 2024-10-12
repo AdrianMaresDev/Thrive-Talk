d6bd8000f7a389e3075aa49bb10b75cebd8df5c1
AdrianMares <adrianmares.dev@gmail.com>
2024-10-12 00:18:22 -0400

    responsive nav bar

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,16 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primaryMint: '#44AF93',
+        darkMint: '#016268',
+        lightMint: '#9FEDD5',
+        lightBlue: '#CEE2E8',
+        primaryBeige: '#F5EFEB',
+        darkBeige: '#9C8881',
+      }
+    },
   },
   plugins: [],
 }
