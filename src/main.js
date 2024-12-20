6394be429de541b0c29b3e83becd01cf06b56af6
AdrianMares <adrianmares.dev@gmail.com>
2024-12-19 23:50:29 -0500

    finished testimonial carousel

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 const menuIcon = document.getElementById('menu-button');
 const closeIcon = document.getElementById('close-button');
 const navMenu = document.getElementById('nav-menu');
+const buttons = document.querySelectorAll('[data-carousel-button]');
+
 
 //Navigation Menu Toggle
 
@@ -14,4 +16,21 @@ closeIcon.addEventListener('click', () => {
     navMenu.classList.add('hidden');
     menuIcon.classList.remove('hidden');
     closeIcon.classList.add('hidden');
+});
+
+//Loop Through Testimonials
+
+buttons.forEach (button => {
+    button.addEventListener('click', () => {
+        const offset = button.dataset.carouselButton === 'next' ? 1 : -1;
+        const slides = button.closest('[data-carousel]').querySelector('[data-slides]');
+
+        const activeSlide = slides.querySelector('[data-active]');
+        let newIndex = [...slides.children].indexOf(activeSlide) + offset;
+        if (newIndex < 0) newIndex = slides.children.length - 1;
+        if (newIndex >= slides.children.length) newIndex = 0;
+        
+        slides.children[newIndex].dataset.active = true;
+        delete activeSlide.dataset.active;
+    })
 });
